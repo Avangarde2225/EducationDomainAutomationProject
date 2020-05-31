7f3c17e68d732c845137048fdf5220ab4b749623
Serdar <serdar2225@gmail.com>
2020-05-30 21:35:36 -0400

    Initial commit

---
+++ src/FunctionalityChecks/EntranceExamAutomation.java
@@ -0,0 +1,25 @@
+package FunctionalityChecks;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class EntranceExamAutomation {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\suler\\Desktop\\Selenium\\chromedriver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://basqar.techno.study/");
+        driver.manage().window().maximize();
+
+        driver.findElement(By.cssSelector("input[placeholder='Username']")).sendKeys("nigeria_tenant_admin");
+        driver.findElement(By.cssSelector("[formcontrolname=\"password\"]")).sendKeys("TnvLOl54WxR75vylop2A");
+        driver.findElement(By.cssSelector("button[aria-label=\"LOGIN\"]")).click();
+
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+
+
+
+    }
+}
