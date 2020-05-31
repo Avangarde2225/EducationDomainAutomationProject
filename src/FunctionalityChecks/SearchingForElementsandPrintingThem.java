7614ef38be3b1e3bb35e324775c3fc83b3c6cea6
Serdar <serdar2225@gmail.com>
2020-05-31 13:55:40 -0400

    element searching and printing last commit

--- src/FunctionalityChecks/SearchingForElementsandPrintingThem.java
+++ src/FunctionalityChecks/SearchingForElementsandPrintingThem.java
@@ -1,9 +1,12 @@
 package FunctionalityChecks;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class SearchingForElementsandPrintingThem {
@@ -26,6 +29,19 @@ public class SearchingForElementsandPrintingThem {
         driver.findElement(By.xpath("//span[contains(text(),'Entrance Exams')]/../..//span[contains(text(),'Setup')]")).click();
         driver.findElement(By.xpath("//span[contains(text(),'Setup')]/../..//span[contains(text(),'Entrance Exams')]")).click();
 
+        //searhing for a particular grade
 
+        driver.findElement(By.xpath("//input[@id='mat-input-4']")).sendKeys("1");
+        driver.findElement(By.xpath("//button[@class='mat-focus-indicator mat-raised-button mat-button-base mat-accent']")).click(); //click on search button
+
+        //printing the search results
+        List<WebElement> searchResults = driver.findElements(By.cssSelector("tbody>tr>td[class='mat-cell cdk-cell cdk-column-name mat-column-name ng-star-inserted']"));
+
+        for (int i = 0; i <searchResults.size() ; i++) {
+            List<WebElement> searchResult = driver.findElements(By.cssSelector("tbody>tr>td[class='mat-cell cdk-cell cdk-column-name mat-column-name ng-star-inserted']"));
+            System.out.println(searchResult.get(i).getText());
+        }
+
+            driver.quit();
     }
 }
