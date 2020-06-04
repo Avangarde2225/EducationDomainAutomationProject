85ffa89e978991f7ad1d4a32d1837be036050f42
Serdar <serdar2225@gmail.com>
2020-06-03 22:38:20 -0400

    Selenium easy form simple testNG framework second test passed

--- src/SeleniumEasyTestNg/SimpleFormTask2TestNg.java
+++ src/SeleniumEasyTestNg/SimpleFormTask2TestNg.java
@@ -4,8 +4,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 import java.util.List;
 
@@ -21,38 +25,32 @@ public class SimpleFormTask2TestNg {
     }
     @BeforeClass
     public void setUp() {
-        System.setProperty( "webdriver.chrome.driver", "C:\\Users\\suler\\Desktop\\Selenium\\chromedriver\\chromedriver.exe" );
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\suler\\Desktop\\Selenium\\chromedriver\\chromedriver.exe");
         driver = new ChromeDriver();
-        driver.get( "https://www.seleniumeasy.com/test/basic-first-form-demo.html" );
+        driver.get("https://www.seleniumeasy.com/test/basic-first-form-demo.html");
         driver.manage().window().maximize();
 
+        WebDriverWait wait = new WebDriverWait(driver, 3);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#at-cv-lightbox")));
+
+        driver.findElement(By.cssSelector("div#at-cv-lightbox-button-holder> a:nth-child(2)")).click();
+    }
+
+        @Test
+        public void test(){
 
         Integer number1 = 100;
         Integer number2 = 222;
 
-        WebElement sum1 = driver.findElement( By.id( "sum1" ) );
-        sum1.sendKeys( number1.toString() );
+        driver.findElement( By.id( "sum1" ) ).sendKeys( number1.toString() );
 
-        WebElement sum2 = driver.findElement( By.id( "sum2" ) );
-        sum2.sendKeys( number2.toString() );
+        driver.findElement( By.id( "sum2" ) ).sendKeys( number2.toString() );
 
-        List<WebElement> buttons = driver.findElements( By.tagName( "button" ) );
-        for(WebElement button : buttons) {
-//            System.out.println(button.getText());
-            if(button.getText().equals( "Get Total" )) {
-                button.click();
-            }
-        }
+        driver.findElement(By.cssSelector("#gettotal > button")).click();
 
-        WebElement display = driver.findElement( By.id( "displayvalue" ) );
-        String text = display.getText();
 
-        if(text.equals( String.valueOf( number1 + number2 ) )) {
-            System.out.println( "Success!" );
-        } else {
-            System.out.println( "Failure!" );
-        }
+    String text = driver.findElement( By.id( "displayvalue" ) ).getText();
 
-        driver.quit();
+    Assert.assertEquals(text, String.valueOf(number1+number2));
     }
 }
